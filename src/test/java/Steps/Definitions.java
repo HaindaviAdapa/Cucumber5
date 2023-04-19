e73d79c196cf21a39fdffd8a6c3cf0bbefecd224
HaindaviAdapa <haindavi1234@gmail.com>
2023-04-19 16:31:10 +0530

    this is new commit

---
+++ src/test/java/Steps/Definitions.java
@@ -0,0 +1,12 @@
+package StepDefinitions1;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Given;
+
+@Given("I want to write a step with preconditiondsfs")
+public void i_want_to_write_a_step_with_preconditiondsfs() {
+		
+	System.out.println("Hello1");
+}
